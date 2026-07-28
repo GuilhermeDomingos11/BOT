152de8543580b11f2ff4e099391aa1564272be10
Guilherme Domingos <58741672+GuilhermeDomingos11@users.noreply.github.com>
2026-07-28 21:28:55 +0100

    Update bot_setup_estrada.py

--- bot_setup_estrada.py
+++ bot_setup_estrada.py
@@ -20,7 +20,7 @@ def run_flask():
     app.run(host='0.0.0.0', port=port)
 
 # 2. CREDENCIAIS
-TOKEN = '8950805985:AAFOKAmSvCoNVeF_UUgvLXOnq8KhFsvD7us'
+TOKEN = '8950805985:AAF0i9-pXNWt47xYFYQaWFAzZMSepQ9G9pc'
 CANAL_ID = '@setupdaestrada'
 LINK_REVOLUT = 'https://revolut.me/guilhevb38'
 USERNAME_BOT = 'oEstafeta_bot' 
